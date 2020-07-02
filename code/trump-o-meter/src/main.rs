19603f90666e7c2ba78071be9fc4688e4a95e0ee
Christian Kauhaus <kc@flyingcircus.io>
2020-07-02 19:41:21 +0200

    Rewrite t-o-m to include more language features

--- code/trump-o-meter/src/main.rs
+++ code/trump-o-meter/src/main.rs
@@ -1,57 +1,62 @@
-use futures::future;
+use futures::future::join_all;
 use serde::Deserialize;
-use std::collections::HashMap;
 use std::error::Error;
-use std::fs;
+use std::fs::File;
+use tokio::runtime::Runtime;
 
+// What we are looking for
 static PERSONS: &[&str] = &["Merkel", "Trump"];
 
-// Count indexed by PERSONS
-type Occurences = Vec<usize>;
-
-// Site name -> URL
 #[derive(Deserialize, Debug)]
-struct Newssites(HashMap<String, String>);
-
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>> {
-    let json = fs::read_to_string("newssites.json")?;
-    let sites: Newssites = serde_json::from_str(&json)?;
-    drop(json);
-    let tasks: Vec<_> = sites.0.iter().map(|(site, url)| print(site, url)).collect();
-    future::join_all(tasks).await;
-    Ok(())
+struct Newssite {
+    name: String,
+    url: String,
 }
 
-async fn check(url: &str) -> Result<Occurences, Box<dyn Error>> {
-    let response = reqwest::get(url).await?;
-    if !response.status().is_success() {
-        return Err(format!(
-            "{}: {}",
-            url,
-            response
-                .status()
-                .canonical_reason()
-                .unwrap_or("Generic server error")
-        )
-        .into());
-    }
-    let body = response.text().await?;
-    Ok(PERSONS
-        .iter()
-        .map(|person| body.matches(person).count())
-        .collect())
+#[derive(Debug)]
+struct Occurences {
+    site: Newssite,
+    // Counts indexed by person
+    counts: Result<Vec<usize>, reqwest::Error>,
 }
 
-async fn print(site: &str, url: &str) {
-    match check(url).await {
-        Ok(counts) => {
-            print!("{:12}", site);
-            for (i, name) in PERSONS.iter().enumerate() {
-                print!(" {:30}", name[0..1].repeat(counts[i]))
+impl Occurences {
+    fn print(&self) {
+        match &self.counts {
+            Ok(counts) => {
+                print!("{:12}", self.site.name);
+                for (n, name) in PERSONS.iter().enumerate() {
+                    print!(" {:30}", name[0..1].repeat(counts[n]))
+                }
+                println!()
             }
-            println!()
+            Err(e) => println!("{:12} Error: {}", self.site.name, e),
         }
-        Err(e) => println!("{:12} Error: {}", site, e),
     }
+}
+
+async fn count_persons(url: &str) -> Result<Vec<usize>, reqwest::Error> {
+    eprintln!("{}: downloading", url);
+    let body = reqwest::get(url).await?.error_for_status()?.text().await?;
+    Ok(PERSONS.iter().map(|p| body.matches(p).count()).collect())
+}
+
+impl Newssite {
+    async fn check(self) -> Occurences {
+        let counts = count_persons(&self.url).await;
+        Occurences { site: self, counts }
+    }
+}
+
+fn run(sites: Vec<Newssite>) -> Vec<Occurences> {
+    let tasks: Vec<_> = sites.into_iter().map(|site| site.check()).collect();
+    Runtime::new().unwrap().block_on(join_all(tasks))
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let sites: Vec<Newssite> = serde_json::from_reader(File::open("newssites.json")?)?;
+    for occ in run(sites) {
+        occ.print();
+    }
+    Ok(())
 }
